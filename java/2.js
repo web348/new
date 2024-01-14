0c09a2b27876f5c25b4665c47fade7ee05c818a8
web348 <147969273+web348@users.noreply.github.com>
2024-01-14 03:51:37 +0000

    datatypes

---
+++ java/2.js
@@ -0,0 +1,6 @@
+const accountId =4321
+let accountEmail ="sam@google.com"
+/* this use for comm in code */
+/*console.log();
+console.table([accountId,accountEmail])
+this is use to print the outputsec one is us to print the data in table form*/
---
+++ java/3.js
@@ -0,0 +1,3 @@
+"use strict"//treat all js code as newer version 
+// ecma is use for learn 
+// null object hai
